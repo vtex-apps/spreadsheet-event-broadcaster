0da3f6626fcc3e16ef9b1f652a66bc36ab71aeda
choque9 <choque.9@gmail.com>
2024-09-02 15:00:31 -0500

    Fix Lint erros

--- node/utils/parsing.ts
+++ node/utils/parsing.ts
@@ -1,14 +1,15 @@
 import type { IncomingMessage } from 'http'
-import Busboy from 'busboy';
+
+import Busboy from 'busboy'
 
 export async function busboyWrapper<T>(req: IncomingMessage): Promise<T> {
-  let result;
-  let clientAppId;
+  let result
+  let clientAppId
 
   try {
-    const parsedData = await parseForm(req);
-    result = parsedData.files;
-    clientAppId = parsedData.fields.appId;
+    const parsedData = await parseForm(req)
+    result = parsedData.files
+    clientAppId = parsedData.fields.appId
   } catch (e) {
     throw new Error(e)
   }
@@ -16,44 +17,44 @@ export async function busboyWrapper<T>(req: IncomingMessage): Promise<T> {
   return {
     fields: { appId: clientAppId },
     files: [result],
-  } as unknown as T;
+  } as unknown as T
 }
 
 async function parseForm(req: IncomingMessage): Promise<{ fields: any, files: any }> {
   try {
     return new Promise((resolve, reject) => {
-      const bb = new Busboy({ headers: req.headers as { 'content-type': string } });
-      const fields: any = {};
-      const files: any = [];
+      const bb = new Busboy({ headers: req.headers as { 'content-type': string } })
+      const fields: any = {}
+      const files: any = []
 
       bb.on('file', (_, file) => {
-        const fileData: Buffer[] = [];
+        const fileData: Buffer[] = []
 
         file.on('data', (data) => {
-          fileData.push(data);
-        });
+          fileData.push(data)
+        })
 
         file.on('end', () => {
-          files.push(Buffer.concat(fileData));
-        });
-      });
+          files.push(Buffer.concat(fileData))
+        })
+      })
 
       bb.on('field', (fieldname, val) => {
-        fields[fieldname] = val;
-      });
+        fields[fieldname] = val
+      })
 
       bb.on('finish', () => {
-        resolve({ fields, files });
-      });
+        resolve({ fields, files })
+      })
 
       bb.on('error', (err: any) => {
-        reject(err);
-      });
+        reject(err)
+      })
 
-      req.pipe(bb);
-    });
+      req.pipe(bb)
+    })
   } catch (error) {
-    throw new Error(error);
+    throw new Error(error)
   }
 }
 
