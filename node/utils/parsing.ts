f510c2eba0ad44ff8d459ed4d1dc3efed6334648
choque9 <choque.9@gmail.com>
2024-09-02 14:49:18 -0500

    Fix bug using busboy. Previous asyncBusboy fails without messages

--- node/utils/parsing.ts
+++ node/utils/parsing.ts
@@ -1,11 +1,60 @@
 import type { IncomingMessage } from 'http'
+import Busboy from 'busboy';
 
-import asyncBusboy from 'async-busboy'
+export async function busboyWrapper<T>(req: IncomingMessage): Promise<T> {
+  let result;
+  let clientAppId;
 
-export async function asyncBusboyWrapper<T>(req: IncomingMessage): Promise<T> {
-  const result = await asyncBusboy(req)
+  try {
+    const parsedData = await parseForm(req);
+    result = parsedData.files;
+    clientAppId = parsedData.fields.appId;
+  } catch (e) {
+    throw new Error(e)
+  }
 
-  return (result as unknown) as T
+  return {
+    fields: { appId: clientAppId },
+    files: [result],
+  } as unknown as T;
+}
+
+async function parseForm(req: IncomingMessage): Promise<{ fields: any, files: any }> {
+  try {
+    return new Promise((resolve, reject) => {
+      const bb = new Busboy({ headers: req.headers as { 'content-type': string } });
+      const fields: any = {};
+      const files: any = [];
+
+      bb.on('file', (_, file) => {
+        const fileData: Buffer[] = [];
+
+        file.on('data', (data) => {
+          fileData.push(data);
+        });
+
+        file.on('end', () => {
+          files.push(Buffer.concat(fileData));
+        });
+      });
+
+      bb.on('field', (fieldname, val) => {
+        fields[fieldname] = val;
+      });
+
+      bb.on('finish', () => {
+        resolve({ fields, files });
+      });
+
+      bb.on('error', (err: any) => {
+        reject(err);
+      });
+
+      req.pipe(bb);
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
 }
 
 export function senderAppIdFromHeaders(headers: IncomingMessage['headers']) {
